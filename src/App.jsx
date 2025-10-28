7a8fd408303b0996029c3ae88b5e467d8c4b122b
tinumalu20-spec <tinumalu20@gmail.com>
2025-10-28 14:52:13 +0530

    intial commit to branch2

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>This is branch1</h2>
+      <h2>This is branch2</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
